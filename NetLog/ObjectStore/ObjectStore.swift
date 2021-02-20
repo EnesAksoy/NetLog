ff5d74df14dfd831ed7957dc171c458413bb7117
Enes Aksoy <enesaksoy1168@gmail.com>
2021-02-21 02:16:18 +0300

    Added objectstore.

---
+++ NetLog/ObjectStore/ObjectStore.swift
@@ -0,0 +1,17 @@
+//
+//  ObjectStore.swift
+//  NetLog
+//
+//  Created by ENES AKSOY on 20.02.2021.
+//  Copyright © 2021 Enes Aksoy. All rights reserved.
+//
+
+import Foundation
+
+class ObjectStore {
+    
+    static let shared = ObjectStore()
+    
+    var selectImage: Data?
+    
+}
