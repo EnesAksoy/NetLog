8d3170e71b4cc62d74f3c3a9ccf5523e93dcc5aa
Enes Aksoy <enesaksoy1168@gmail.com>
2021-02-20 15:19:40 +0300

    first commit

---
+++ NetLog/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NetLog
+//
+//  Created by ENES AKSOY on 20.02.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
