2920a66facaa72acf14be4706967ef0c5a236f6b
Enes Aksoy <enesaksoy1168@gmail.com>
2021-02-21 02:14:22 +0300

    Added UIViewController Extension.

---
+++ NetLog/Common/Extension/UIViewControllerExtension.swift
@@ -0,0 +1,20 @@
+//
+//  UIViewControllerExtension.swift
+//  NetLog
+//
+//  Created by ENES AKSOY on 20.02.2021.
+//  Copyright © 2021 Enes Aksoy. All rights reserved.
+//
+
+import UIKit
+
+
+extension UIViewController {
+    func createAlert(message: String, title: String) {
+        let alertController = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        let buttonTitle = NSLocalizedString("AlertButtonTitle", comment: "")
+        let OKAction = UIAlertAction(title: buttonTitle, style: .default, handler: nil)
+        alertController.addAction(OKAction)
+        self.present(alertController, animated: true, completion: nil)
+    }
+}
